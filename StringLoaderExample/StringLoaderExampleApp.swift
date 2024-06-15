2c2ad794b17630a73dc6011cdcd87863843f2664
rizkym <rizky_mashudy@yahoo.co.id>
2024-06-16 01:16:56 +0700

    Initial Commit

---
+++ StringLoaderExample/StringLoaderExampleApp.swift
@@ -0,0 +1,20 @@
+//
+//  StringLoaderExampleApp.swift
+//  StringLoaderExample
+//
+//  Created by Rizky Mashudi on 16/06/24.
+//
+
+import SwiftUI
+
+@main
+struct StringLoaderExampleApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
